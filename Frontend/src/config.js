8d054673c55c51e07a017c89035dfd61b7929d15
22Bhargavi <nathbhargavi.bn@gmail.com>
2024-05-31 23:11:06 +0530

    configset

--- Frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-
-module.exports = {
-  fetchAPIstring:
-    "https://cs-2-skin-price-fetcher-api.vercel.app/api/fetchSkinData",
-  saveAPIstring:
-    "https://cs-2-skin-price-fetcher-api.vercel.app/api/saveSkinData",
-  skinDetailsAPIstring: "https://bymykel.github.io/CSGO-API/api/en/skins.json",
-};
